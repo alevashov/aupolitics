829f9307e9b6fe69b8a8347cfba7a26e576c7902
Alex Levashov <alex.levashov@gmail.com>
2018-12-07 04:24:18 +0000

    app changes

--- AUPolVizual/app.R
+++ AUPolVizual/app.R
@@ -1,31 +1,42 @@
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(tidyverse)
 library(rtweet)
 library(ggthemes)
 library(RMySQL)
+library(lubridate)
 # setting DB connection and getting users and tweets
+setwd("~/aupolitics")
+source("dbconnectiondetails.R")
 # note it may take a while to load tweets
-# source("dbconnectiondetails.R")
-# con <- dbConnect(RMySQL::MySQL(), dbname= db_name, username=user, password=pw, host=host, port=3306)
-# users <- dbReadTable(con, 'users2')
-# tweets <- dbReadTable(con, 'tweets2')
-# dbDisconnect(con)
-up <- select(users, one_of(c("user_id", "party")))
-t <- left_join(tweets, up, by="user_id")
+# implement conditional update logic, only update once per day
+
+# define update as a function
+updateDb <- function (){
+con <- dbConnect(RMySQL::MySQL(), dbname= db_name, username=user, password=pw, host=host, port=3306)
+t <- dbReadTable(con, 'tweets_party')
+save(t, file="tweets_app.RData")
+dbDisconnect(con)
+}
+
+# load tweets from local file and compare by status_id with Db
+load("tweets_app.RData")
+max_local <- as.numeric(max(t$status_id))
+con <- dbConnect(RMySQL::MySQL(), dbname= db_name, username=user, password=pw, host=host, port=3306)
+max_db <- con %>% dbSendQuery("SELECT MAX(status_id) FROM twitterr.tweets_party;") %>% dbFetch()
+dbDisconnect(con)
+max_db <- as.numeric(max_db[1])
+if (max_local < max_db) updateDb()
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Australian Member of Parliament Twitter stats"),
+   titlePanel("Australian Member of Parliament Twitter statistics"),
    
    # Sidebar with a selector for party
    sidebarLayout(
@@ -52,7 +63,7 @@ server <- function(input, output) {
            # draw the histogram with the specified number of bins
            clients %>% ggplot(aes(reorder(source,count),count, fill=source, label = count))+
                    geom_col()+
-                   labs(y="Number of tweets", fill="Source", title = "Twitter sources/devices")+
+                   labs(x="", y="Number of tweets", fill="Source", title = "Twitter sources/devices")+
                    geom_text(size = 3, position = position_stack(vjust = 0.5))+
                    theme_dark()
       
